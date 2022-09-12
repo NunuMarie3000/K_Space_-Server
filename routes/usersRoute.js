42ea4c2577770299273e6d1d19390e90ed6788d5
Storm O'Bryant <vmarie1997@gmail.com>
2022-09-12 16:18:33 -0500

    slight edit

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,12 +1,23 @@
 // this route needs to get all users
 const express = require('express')
 const router = express.Router()
-const userModel = require('../models/userModel')
+const user = require('../models/userModel')
 
 router.get('/', async (req,res)=>{
   try {
-    const allUsers = await userModel.find({})
+    const allUsers = await user.userModel.find({})
     res.status(200).send(allUsers)
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
+// will be public, come back to this
+router.get('/:id', async (req,res)=>{
+  const userId = req.params.id
+  try {
+    const publicUser = await user.userModel.findById(userId)
+    res.status(200).send(publicUser)
   } catch (error) {
     console.log(error.message)
   }
